998a6588ab4f964434089bad60ed4681ed6470a2
LB <78799392+lbam@users.noreply.github.com>
2022-05-04 15:43:11 +0200

    Remove testing endpoints

--- src/apikeys/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    #path("", views.index, name="index"),
-    path("<email>/", views.key_for_email),
-]
--- src/apikeyserv/urls.py
+++ src/apikeyserv/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("apikeys/", include("apikeys.urls")),
     path("signingkeys/", include("signingkeys.urls")),
 ]
