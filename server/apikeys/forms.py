f9060633a235f22f2305b6a8dda8f7b8b7f21871
LB <78799392+lbam@users.noreply.github.com>
2023-05-30 14:50:16 +0200

    Add application to form field "Organisation"

--- server/apikeys/forms.py
+++ server/apikeys/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class RequestForm(forms.Form):
-    organisation = forms.CharField(max_length=256)
+    organisation = forms.CharField(max_length=256,
+                                   label="Organisation/application")
     email = forms.CharField(max_length=256)
     reason = forms.CharField(max_length=512)
