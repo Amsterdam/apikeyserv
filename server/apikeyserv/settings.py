c5cbff6f5b077ab297aaf5774b32ce85ef0e14c1
Jan Murre <jan.murre@gmail.com>
2023-07-10 17:08:18 +0200

    Add improvements to styling and configurability (#10)
    * Make the form a little more pleasant to the eye.
    
    * Add stub for REST API.
    
    Maybe verkenner prefers a REST interface.
    
    * Make the static files work for local development.
    
    * Add configurable settings for STATIC_URL and FORCE_SCRIPT_NAME
    
    * Bump django to 4.2.3
    
    Django 4.2.1 has a vulnerability

--- server/apikeyserv/settings.py
+++ server/apikeyserv/settings.py
@@ -141,10 +141,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/apikeyserv/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = env.get("STATIC_URL", "static/")
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# To run on a subpath, we need to define this variable.
+FORCE_SCRIPT_NAME = env.get('FORCE_SCRIPT_NAME')
