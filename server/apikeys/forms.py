f6421ce583909cb64edf81e71836ef2a44279faa
jjmurre <jan.murre@gmail.com>
2023-07-12 22:00:43 +0200

    Add extra checkbox to form for agreement on handling of personal data
    This data is only validated, not stored in the database.

--- server/apikeys/forms.py
+++ server/apikeys/forms.py
@@ -4,6 +4,7 @@ from apikeys.models import ApiKey
 
 
 class RequestForm(forms.ModelForm):
+    agree_on_personal_data = forms.BooleanField(required = True)
     class Meta:
         model = ApiKey
         exclude = ["id", "created", "modified", "expires", "sent"]
